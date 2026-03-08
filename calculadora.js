0daf15f1d53f7c8c4ee4fef1ec2f895a4ff5f208
ToniDev <tonigstm96@gmail.com>
2026-03-08 22:45:31 +0100

    Funcional - diseño - limpiar codigo y desplegar en pages

--- calculadora.js
+++ calculadora.js
@@ -6,10 +6,22 @@ let igual = document.getElementById("equals");
 let punto = document.getElementById("dot");
 let unOperador = false;
 let numDecimal = false;
+let bombHasBeenPlanted = new Audio('media/audio/bomb.mp3')
+let iniciarBomba = new Audio('media/audio/open.mp3');
+let sonidoBoton = new Audio('media/audio/boton.mp3');
+let cajaCalculadora = document.querySelector('.container');
+let todosBotones = document.querySelectorAll('input[type="button"]');
+let defusar = new Audio('media/audio/defuse.mp3');
+let botonMapa = document.getElementById('btn-mapa');
+
 
 borrar.addEventListener("click", limpiarPantalla, false);
 igual.addEventListener("click", calcular, false);
 punto.addEventListener("click", decimal, false);
+cajaCalculadora.addEventListener("click", function(){
+    iniciarBomba.play();
+}, {once: true});
+botonMapa.addEventListener("click", cambiarMapa, false);
 
 for (let i = 0; i < boton.length; i++) {
 console.log(boton[i]);
@@ -19,6 +31,10 @@ boton[i].addEventListener("click", valor, false);
 for (let i = 0; i <operador.length; i++) {
 console.log(operador[i]);
 operador[i].addEventListener("click", operacion, false);
+}
+
+for (let i = 0; i< todosBotones.length; i++){
+    todosBotones[i].addEventListener("click", reproducirBoton, false);
 }
 
 function valor(e){
@@ -33,7 +49,8 @@ function decimal(e){
     else if (!numDecimal){
         pantalla.value += e.target.value;
         numDecimal = true;
-    } else {
+    } 
+    else {
         alert("Solo puede introducir el punto una vez por número!");
     }
 }
@@ -46,7 +63,8 @@ function operacion(e){
         pantalla.value += e.target.value;
         unOperador = true;
         numDecimal = false;
-    } else {
+    } 
+    else {
         alert("Introduzca solo un operador!");
     }  
 }
@@ -54,13 +72,33 @@ function operacion(e){
 function limpiarPantalla(e) {
     pantalla.value = "";
     unOperador = false;
+    numDecimal = false;
+    defusar.play();
 }
 
 function calcular(e) {
     if (unOperador) {
         alert("Introduzca una operación correcta!")
     }
-    pantalla.value = eval(pantalla.value);
-    unOperador = false;
-    numDecimal = false;
+    else {  
+        pantalla.value = eval(pantalla.value);
+        unOperador = false;
+        numDecimal = false;
+        bombHasBeenPlanted.play();
+    }
+}
+
+function reproducirBoton() {
+    sonidoBoton.currentTime = 0;
+    sonidoBoton.play();
+}
+
+function cambiarMapa(){
+    document.body.classList.toggle('assault');
+    if(document.body.classList.contains('assault')) {
+        botonMapa.value = "Ir a de_dust2";
+    }
+    else{
+        botonMapa.value = "Ir a cs_assault";
+    }
 }
