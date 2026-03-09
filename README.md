b2eaaa2f50ede7291a6dd77950411751d1eba0d6
ToniDev <tonigstm96@gmail.com>
2026-03-09 10:43:32 +0100

    Revisado y finalizado

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculadora CS 1.6
+Pequeño proyecto de calculadora interactiva para la asignatura Lenguaje de Marcas inspirado en la bomba y menús de Counter Strike 1.6
--- js/calculadora.js
+++ js/calculadora.js
@@ -1,42 +1,49 @@
+/*Primero se han definido las variables para identificarlas en el DOM, la pantalla, botones, variables para señalar estados y audios 
+que sonarán de forma interactiva.*/
 let pantalla = document.getElementById("screen");
 let boton = document.getElementsByClassName("btn");
 let operador = document.getElementsByClassName("operator");
 let borrar = document.getElementById("erase");
 let igual = document.getElementById("equals");
 let punto = document.getElementById("dot");
+let cajaCalculadora = document.querySelector('.container');
+let todosBotones = document.querySelectorAll('input[type="button"]');
+let botonMapa = document.getElementById('btn-mapa');
+let linkGithub = document.getElementById('link-github');
 let unOperador = false;
 let numDecimal = false;
 let bombHasBeenPlanted = new Audio('media/audio/bomb.mp3')
 let iniciarBomba = new Audio('media/audio/open.mp3');
 let sonidoBoton = new Audio('media/audio/boton.mp3');
-let cajaCalculadora = document.querySelector('.container');
-let todosBotones = document.querySelectorAll('input[type="button"]');
 let defusar = new Audio('media/audio/defuse.mp3');
-let botonMapa = document.getElementById('btn-mapa');
+let disparadEnLaBrecha = new Audio('media/audio/fire.mp3');
 
-
+/*Después se han añadido los event listeners para que al clicar un botón, enlace o incluso un contenedor llame a una función concreta
+ */
 borrar.addEventListener("click", limpiarPantalla, false);
 igual.addEventListener("click", calcular, false);
 punto.addEventListener("click", decimal, false);
-cajaCalculadora.addEventListener("click", function(){
-    iniciarBomba.play();
-}, {once: true});
+cajaCalculadora.addEventListener("click", inicioBomba, {once: true});
 botonMapa.addEventListener("click", cambiarMapa, false);
+linkGithub.addEventListener("click", fireInDaHole, false);
 
+/*Bucles for que añaden a cada botón un event listener numerado con el índice de un bucle for, ya que get elements by class name 
+da una colección
+ */
 for (let i = 0; i < boton.length; i++) {
-console.log(boton[i]);
-boton[i].addEventListener("click", valor, false);
+    boton[i].addEventListener("click", valor, false);
 }
 
-for (let i = 0; i <operador.length; i++) {
-console.log(operador[i]);
-operador[i].addEventListener("click", operacion, false);
+for (let i = 0; i < operador.length; i++) {
+    operador[i].addEventListener("click", operacion, false);
 }
 
 for (let i = 0; i< todosBotones.length; i++){
     todosBotones[i].addEventListener("click", reproducirBoton, false);
 }
 
+/*Funciones para el funcionamiento de la calculadora. Utilizan el parámetro e que representa el evento concreto (un click)
+ */
 function valor(e){
     pantalla.value += e.target.value;
     unOperador = false;
@@ -88,11 +95,23 @@ function calcular(e) {
     }
 }
 
+/*Funciones que reproducen sonidos de CS 1.6
+ */
+function inicioBomba(){
+    iniciarBomba.play();
+}
+
 function reproducirBoton() {
-    sonidoBoton.currentTime = 0;
+    sonidoBoton.currentTime = 0; //Con currentTime = 0 el sonido se reinicia cuando volvemos a llamar a la función, así no se solapa el sonido de pulsar botones.
     sonidoBoton.play();
 }
 
+function fireInDaHole(){
+    disparadEnLaBrecha.play();
+}
+
+/*Toggle para cambiar al "modo noche", representado por un mapa de Counter Strike más oscuro. Cambia la clase para que el css actúe de forma distina.
+ */
 function cambiarMapa(){
     document.body.classList.toggle('assault');
     if(document.body.classList.contains('assault')) {
@@ -102,3 +121,5 @@ function cambiarMapa(){
         botonMapa.value = "Ir a cs_assault";
     }
 }
+
+
